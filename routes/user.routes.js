52a38e5b409f328f3fbb41c05ceeaf3b9864187b
Vishnu-vkn <vishnuvkn204@gmail.com>
2026-02-15 23:16:13 +0530

    db connected and created userdb schema

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,16 +1,39 @@
 const express = require("express");
 const router = express.Router();
+const { body, validationResult } = require("express-validator");
+const userModel = require("../models/user.model")
 
 
-/*  /user/test */   
-router.get('/register',(req,res)=>{
+/*  /user/test */
+router.get('/register', (req, res) => {
     res.render("register");
 
 })
 
-router.post('/register',(req,res)=>{
-    console.log(req.body);
-    res.send("User registered");
+router.post('/register', 
+    body('email').trim().isEmail().isLength({min:13}),
+    body('username').trim().isLength({min:5}),
+    body('password').trim().isLength({min:5}),
+    async (req, res) => {
+
+    const errors= validationResult(req);
+
+    if(!errors.isEmpty()){
+        return res.status(400).json({
+            errors:errors.array(),
+            message:"Invalid data"
+        })
+    }
+    
+    const  {email , username , password} = req.body;
+     
+    const newUser = await userModel.create({
+        email,
+        username, 
+        password
+    });
+
+    res.json(newUser)
 })
 
-module.exports=router; 
+module.exports = router; 
